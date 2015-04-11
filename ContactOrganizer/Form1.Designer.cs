fc1a3d1c7d1b829959e14b4945bf9b02ea81c29d
Fzsarq <peterliu930@gmail.com>
2015-04-10 23:23:54 -0400

    Initial Commit
    Nearly Finished.
    Missing: Search and Keypress Validation

---
+++ ContactOrganizer/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ContactOrganizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            this.cmdInsert = new System.Windows.Forms.Button();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.lblCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dg1
+            // 
+            this.dg1.AllowUserToAddRows = false;
+            this.dg1.AllowUserToDeleteRows = false;
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.Location = new System.Drawing.Point(270, 9);
+            this.dg1.Name = "dg1";
+            this.dg1.ReadOnly = true;
+            this.dg1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dg1.Size = new System.Drawing.Size(463, 164);
+            this.dg1.TabIndex = 0;
+            // 
+            // cmdInsert
+            // 
+            this.cmdInsert.Location = new System.Drawing.Point(15, 138);
+            this.cmdInsert.Name = "cmdInsert";
+            this.cmdInsert.Size = new System.Drawing.Size(236, 38);
+            this.cmdInsert.TabIndex = 1;
+            this.cmdInsert.Text = "Add New Contact";
+            this.cmdInsert.UseVisualStyleBackColor = true;
+            this.cmdInsert.Click += new System.EventHandler(this.cmdInsert_Click);
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.Location = new System.Drawing.Point(270, 195);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(236, 38);
+            this.cmdDelete.TabIndex = 3;
+            this.cmdDelete.Text = "Delete Contact";
+            this.cmdDelete.UseVisualStyleBackColor = true;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Last Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone No.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email Address";
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(97, 9);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(154, 20);
+            this.txtLName.TabIndex = 8;
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(97, 39);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(154, 20);
+            this.txtFName.TabIndex = 9;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(97, 70);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(154, 20);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(97, 96);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(154, 20);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.Location = new System.Drawing.Point(15, 195);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(236, 38);
+            this.cmdUpdate.TabIndex = 12;
+            this.cmdUpdate.Text = "Save Contact";
+            this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(593, 180);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 13);
+            this.lblCount.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 245);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.cmdUpdate);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdDelete);
+            this.Controls.Add(this.cmdInsert);
+            this.Controls.Add(this.dg1);
+            this.Name = "Form1";
+            this.Text = "Contact Organizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dg1;
+        private System.Windows.Forms.Button cmdInsert;
+        private System.Windows.Forms.Button cmdDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button cmdUpdate;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ ContactOrganizer/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ContactOrganizer
+{
+    //We are using the ADO.NET Connected Model(DataReader) as opposed to the Disconnected Model(DataSet). Easier to manage when Inserting, updating and deleting data.
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConn = null;
+        private SqlCommand sqlCmd = null;
+        private string origLName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Get data from table and fill dg1. If table is empty, a message box will pop up.
+            retrieveTData();
+            dg1.Click += dg1_Click;
+            clearText();
+            control("i");
+        }
+
+        void dg1_Click(object sender, EventArgs e)
+        {
+            dg1.CurrentRow.Selected = true;
+            origLName = dg1.CurrentRow.Cells[1].Value.ToString();
+            txtLName.Text = dg1.CurrentRow.Cells[1].Value.ToString();
+            txtFName.Text = dg1.CurrentRow.Cells[2].Value.ToString();
+            txtPhone.Text = dg1.CurrentRow.Cells[3].Value.ToString();
+            txtEmail.Text = dg1.CurrentRow.Cells[4].Value.ToString();
+            control("c");
+        }
+
+
+        //This method will be called on every button click. (Will rebind the data to dg1)
+        //Gets data from the database and fills the dg1 with said data
+        private void retrieveTData()
+        {
+            string connStr = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=C:\\PROG37721\\ContactOrganizer\\ContactOrganizer\\Clients.mdf;Integrated Security=True";
+            string select = "SELECT * FROM [tContacts]";
+
+            try
+            {
+                sqlConn = new SqlConnection(connStr);
+                sqlConn.Open();
+                sqlCmd = new SqlCommand();
+                sqlCmd.Connection = sqlConn;
+                sqlCmd.CommandText = select;
+                SqlDataReader sDr = sqlCmd.ExecuteReader();
+
+                if (sDr.HasRows)
+                {
+                    bindingSource1.DataSource = sDr;
+                    dg1.DataSource = bindingSource1;
+                    dg1.ClearSelection();
+                    lblCount.Text = "Number of Contacts: " + dg1.Rows.Count.ToString();
+                }
+                    
+                else
+                {
+                    lblCount.Text = "Number of Contacts: 0";
+                    MessageBox.Show("Table is empty", "Read Result");
+                }
+
+                sDr.Close();
+                sqlConn.Close();
+            }
+            catch (SqlException ex)
+            {
+                if (sqlConn != null)
+                {
+                    sqlConn.Close();
+                }
+                MessageBox.Show(ex.Message, "An error occurred while reading the Database");
+            }
+        }
+
+        private void cmdInsert_Click(object sender, EventArgs e)
+        {
+            if (validInput())
+            {
+                if (validFullName("i"))
+                {
+                    string insert = "INSERT INTO [tContacts] ([lname], [fname], [phone], [email]) VALUES ('" + txtLName.Text + "','" + txtFName.Text + "','" + txtPhone.Text + "','" + txtEmail.Text + "')";
+
+                    try
+                    {
+                        sqlConn.Open();
+                        sqlCmd.CommandText = insert;
+                        sqlCmd.ExecuteNonQuery();
+                        sqlConn.Close();
+                        retrieveTData();
+                        clearText();
+                    }
+                    catch (SqlException ex)
+                    {
+                        if (sqlConn != null)
+                        {
+                            sqlConn.Close();
+                        }
+                        MessageBox.Show(ex.Message, "Error Inserting Record");
+                    }
+                }
+            }
+        }
+
+        private void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            if (validInput())
+            {
+                if (validFullName("u"))
+                {
+                    string update = 
+                        "UPDATE [tContacts] SET [lname] = '" + txtLName.Text 
+                        + "', [fname] = '" + txtFName.Text 
+                        + "', [phone] = '" + txtPhone.Text 
+                        + "', [email] = '" + txtEmail.Text
+                        + "' WHERE [lname] = '" + origLName + "'";
+
+                    try
+                    {
+                        sqlConn.Open();
+                        sqlCmd.CommandText = update;
+                        sqlCmd.ExecuteNonQuery();
+                        sqlConn.Close();
+                        retrieveTData();
+                        clearText();
+                    }
+                    catch (SqlException ex)
+                    {
+                        if (sqlConn != null)
+                        {
+                            sqlConn.Close();
+                        }
+                        MessageBox.Show(ex.Message, "Error Updating Record");
+                    }
+                    control("i");
+                }
+            }
+        }
+
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to delete this record?", "Delete Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == System.Windows.Forms.DialogResult.Yes)
+            {
+                string delete = "DELETE FROM [tContacts] WHERE [lname] = '" + origLName + "'";
+
+                try
+                {
+                    sqlConn.Open();
+                    sqlCmd.CommandText = delete;
+                    sqlCmd.ExecuteNonQuery();
+                    sqlConn.Close();
+                    //This removes the selected row if only one row remains in dg1. If there is only one row in the table, that row will persist after deletion because 
+                    //if (dg1.Rows.Count < 2)
+                    //{
+                        dg1.Rows.RemoveAt(dg1.CurrentRow.Index);
+                    //}
+                    retrieveTData();
+                    clearText();
+
+                }
+                catch (SqlException ex)
+                {
+                    if (sqlConn != null)
+                    {
+                        sqlConn.Close();
+                    }
+
+                    MessageBox.Show(ex.Message, "Error Deleting Record");
+                }
+            }
+            control("i");
+        }
+
+        private bool validInput()
+        {
+            if (txtLName.Text.Length < 1)
+            {
+                MessageBox.Show("Last name field cannot be empty", "Missing Last Name", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtLName.Focus();
+                return false;
+            }
+
+            if (txtLName.Text.Length < 2)
+            {
+                MessageBox.Show("Last name must have 2 or more characters", "Invalid Last Name", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtLName.Focus();
+                return false;
+            }
+
+            if (txtFName.Text.Length < 1)
+            {
+                MessageBox.Show("First name field cannot be empty", "Missing First Name", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtFName.Focus();
+                return false;
+            }
+
+            if (txtFName.Text.Length < 2)
+            {
+                MessageBox.Show("First name must have 2 or more characters", "Invalid Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtFName.Focus();
+                return false;
+            }
+            
+            return true;
+        }
+
+        private bool validFullName(string state)
+        {
+            if (state.Equals("i"))
+            {
+                for (int i = 0; i < dg1.Rows.Count; i++)
+                {
+                    if (txtLName.Text.ToLower().Equals(dg1.Rows[i].Cells[1].Value.ToString().ToLower()) &&
+                        txtFName.Text.ToLower().Equals(dg1.Rows[i].Cells[2].Value.ToString().ToLower()))
+                    {
+                        MessageBox.Show("This contact already exists. 2 contacts cannot share the same first and last name", "Entry Violation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtLName.Focus();
+                        return false;
+                    }
+                }
+            }
+            else if (state.Equals("u"))
+            {
+                for (int i = 0; i < dg1.Rows.Count; i++)
+                {
+                    if (i != dg1.CurrentRow.Index)
+                    {
+                        if (txtLName.Text.ToLower().Equals(dg1.Rows[i].Cells[1].Value.ToString().ToLower()) &&
+                            txtFName.Text.ToLower().Equals(dg1.Rows[i].Cells[2].Value.ToString().ToLower()))
+                        {
+                            MessageBox.Show("This contact already exists. 2 contacts cannot share the same first and last name", "Entry Violation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            txtLName.Focus();
+                            return false;
+                        }
+                        if (txtEmail.Text.ToLower().Equals(dg1.Rows[i].Cells[4].Value.ToString().ToLower()))
+                        {
+                            MessageBox.Show("This email already exists. Multiple contacts cannot share the same email", "Entry Violation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            txtEmail.Focus();
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void clearText()
+        {
+            txtLName.Text = "";
+            txtFName.Text = "";
+            txtPhone.Text = "";
+            txtEmail.Text = "";
+            txtLName.Focus();
+            dg1.ClearSelection();
+        }
+
+        private void control(String control)
+        {
+            if (control.Equals("i"))
+            {
+                cmdInsert.Enabled = true;
+                cmdUpdate.Enabled = false;
+                cmdDelete.Enabled = false;
+            }
+            else if (control.Equals("c"))
+            {
+                cmdInsert.Enabled = false;
+                cmdUpdate.Enabled = true;
+                cmdDelete.Enabled = true;
+            }
+        }
+    }
+}
